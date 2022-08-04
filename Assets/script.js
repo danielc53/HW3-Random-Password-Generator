28aa56d372b18b171429b6fb2bc6312ed85afa05
danielc53 <dannyc53@hotmail.com>
2022-08-04 14:38:26 -0400

    reordered js logic

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,20 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 //empty array to store generatePassword method
 var characterLength = [];
 
 //empty array for storing prompt choices
-var choiceArr = [];
-
-//ALL CHARACTERS AVAILABLE FOR PASSWORD GENERATION
-var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','_','+','?','<','\'','>',':','{',',','}','[',']','`','~','|','_','\\','\"','\''];
-var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var choiceArray = [];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  //if getPrompts returns true, generate new password
-  var truePrompts = getPrompts();
-  if (truePrompts) {
+  //if checkPrompts returns true, generate new password
+  var validPrompts = checkPrompts();
+  if (validPrompts) {
     var newPassword = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = newPassword;
@@ -30,25 +24,16 @@ function writePassword() {
   }
 }
 
-
-//generates
-function generatePassword() {
-  var password = "";
-  //keep repeating fucntion until the specified characterLength criteria is met
-  for(var i = 0; i < characterLength; i++) {
-    //math.floor will round to an integer after multiplying a random number between 0 and 1 to the value assigned to the choiceArr
-    var randomCharacter = Math.floor(Math.random()*choiceArr.length);
-    //password is redefined 
-    password = password + choiceArr[randomCharacter];
-  }
-  return password;
-}
+//ALL CHARACTERS AVAILABLE FOR PASSWORD GENERATION
+var specialCharArray = ['!','@','#','$','%','^','&','*','(',')','_','+','?','<','\'','>',':','{',',','}','[',']','`','~','|','_','\\','\"','\''];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
-
-function getPrompts(){
+function checkPrompts(){
   //redefine choiceArr to 'clear' the string if any value has been applied to it previously
-  choiceArr = [];
+  choiceArray = [];
   characterLength = prompt("Pick a password length between 8 and 128 characters (numbers ONLY)");
 
   //prompt will only accept an integer value between 8 and 128
@@ -58,19 +43,35 @@ function getPrompts(){
   }
 
   if (confirm("Include lowercase characters?")) {
-    choiceArr = choiceArr.concat(lowerCaseArr);
+    choiceArray = choiceArray.concat(lowerCaseArray);
   }
 
   if (confirm("Include uppercase characters?")) {
-    choiceArr = choiceArr.concat(upperCaseArr);
+    choiceArray = choiceArray.concat(upperCaseArray);
   }
 
   if (confirm("Include special characters?")) {
-    choiceArr = choiceArr.concat(specialCharArr);
+    choiceArr = choiceArray.concat(specialCharArray);
   }
 
   if (confirm("Include numerical characters?")) {
-    choiceArr = choiceArr.concat(numberArr);
+    choiceArray = choiceArray.concat(numberArray);
   }
   return true;
 }
+
+//generates
+function generatePassword() {
+  var password = "";
+  //keep repeating fucntion until the specified characterLength criteria is met
+  for(var i = 0; i < characterLength; i++) {
+    //math.floor will round to an integer after multiplying a random number between 0 and 1 to the value assigned to the choiceArr
+    var randomCharacter = Math.floor(Math.random()*choiceArray.length);
+    //password is redefined 
+    password = Password + choiceArray[randomCharacter];
+  }
+  return password;
+}
+
+
+
